f179293bb912d132a6aa50f6f3ce5c14202c5104
Joshan Dasireddy <joshan.dasireddy@argano.com>
2023-03-14 12:07:59 +0530

    project after adding time stamp

---
+++ src/main/java/com/javatechie/crud/entity/Order.java
@@ -0,0 +1,51 @@
+package com.javatechie.crud.entity;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Table(name = "ORDERS_TBL")
+public class Order {
+    public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public int getQty() {
+		return qty;
+	}
+	public void setQty(int qty) {
+		this.qty = qty;
+	}
+	public double getPrice() {
+		return price;
+	}
+	public void setPrice(double price) {
+		this.price = price;
+	}
+	public Date getDate() {
+		return date;
+	}
+	public void setDate(Date date) {
+		this.date = date;
+	}
+	@Id
+    @GeneratedValue
+    private int id;
+    private String name;
+    private int qty;
+    private double price;
+    @CreationTimestamp
+    private Date date;
+    //----
+}
